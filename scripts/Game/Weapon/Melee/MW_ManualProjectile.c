3e4b57d41718c6218063801c267c9f6c7e6f989d
Alessandro <alessandropicchetti@hotmail.it>
2022-06-24 17:23:05 +0200

    cleaned

--- scripts/Game/Weapon/Melee/MW_ManualProjectile.c
+++ scripts/Game/Weapon/Melee/MW_ManualProjectile.c
@@ -4,26 +4,12 @@ class MW_ManualProjectileClass : ProjectileClass
 
 class MW_ManualProjectile : Projectile 
 {
-	
-	ShellMoveComponent shell;		//to modify stuff in real time
 	Physics addedPhysics;
 	const ResourceName replaceEntityResourceName = "{81D22DE5FB6B4414}TEST2/FirstWorkingThrowingKnife.et";
 
 	override void EOnInit(IEntity owner)
 	{
-		//Print("Projectile Mele");
-		
-		
-		
-		// we need to ignore the player... later I guess
 		SetEventMask(EntityEvent.INIT | EntityEvent.SIMULATE | EntityEvent.CONTACT);
-		
-		shell = ShellMoveComponent.Cast(FindComponent(ShellMoveComponent));		//everything is hidden in here... not sure if I can actually manage stuff with it.
-		//Physics test = owner.GetPhysics();
-		//test.Destroy();
-		//owner.GetPhysics().Destroy();
-
-
 	}
 
 	
@@ -43,18 +29,12 @@ class MW_ManualProjectile : Projectile
 		
 		if (!tempFixAlreadySpawned && other.Type() == GenericTerrainEntity)
 		{
-			Print("Terrain");
-			
-			
-			
+			//Print("Terrain");
 			IEntity newWeapon = GetGame().SpawnEntityPrefab(replaceEntityResource, GetGame().GetWorld());
-			
 			vector currentTransform[4];
 			GetTransform(currentTransform);
 			
-			
 			newWeapon.SetTransform(currentTransform);
-			
 			delete owner;
 			
 		}
@@ -64,20 +44,10 @@ class MW_ManualProjectile : Projectile
 		if (!tempFixAlreadySpawned && other.FindComponent(CharacterControllerComponent) && currentPlayer != other)
 		{
 			CharacterControllerComponent cntrlComp = CharacterControllerComponent.Cast(other.FindComponent(CharacterControllerComponent));
-			//cntrlComp.ForceDeath();
-
-			
 			ChimeraCharacter chimeraOther = ChimeraCharacter.Cast(other);
 			SCR_DamageManagerComponent damageManagerComponent = chimeraOther.GetDamageManager();
-
-
-			
-			
 			array<HitZone> hitZones = {};
 			damageManagerComponent.GetPhysicalHitZones(hitZones);
-			
-			
-
 			vector hitzoneTransform[4];
 			
 			int index = 0;		//??
@@ -103,26 +73,15 @@ class MW_ManualProjectile : Projectile
 			{
 			
 				hitzone.TryGetColliderDescription(other, index, hitzoneTransform, currentBoneIndex, currentNodeID);
-				
 				vector worldTransform = other.CoordToParent(hitzoneTransform[1]);
-				
-				
-				
-				
+
 				float currentDifference = Math.AbsFloat(contact.Position[0] - worldTransform[0]);
 				if (currentDifference < 2)
 				{
-					
-					
-					
+
 					if (bestDifference == -1)
-					{
 						bestDifference = currentDifference;
 
-					
-					}
-				
-					
 					if (bestDifference > currentDifference)
 					{
 						bestDifference = currentDifference;
@@ -130,36 +89,18 @@ class MW_ManualProjectile : Projectile
 						correctBoneIndex = currentBoneIndex;
 						bestHitzone = ScriptedHitZone.Cast(hitzone);
 
-
-					
 					}
 
-					//Print(bestDifference);
-					//bestDifference = Math.AbsFloat(contact.Position[0] - worldTransform[0];
-					
-					
-					//Print(currentNodeID);
 				}
 				
-				
-				
-				//Print(hitzoneTransform[0]);
 			}
-			//Print(correctNodeID);
-			//Print("_____________");
-						
-			//deletes the knife and apply a mesh on the char 
-			
+
 			if (correctNodeID != 0)
 			{
 
 				IEntity spawnedKnife = GetGame().SpawnEntityPrefab(replaceEntityResource);
-				//Print(spawnedKnife.GetOrigin());
-				
 				float damage = Math.RandomFloat(30, 60);
 				damageManagerComponent.HandleDamage(EDamageType.KINETIC, damage, contact.Normal, owner, bestHitzone, spawnedKnife, contact.Material1 , correctBoneIndex, correctNodeID);
-			
-				
 				vector correctedCoord = other.CoordToLocal(contact.Position);
 				
 				
@@ -184,92 +125,17 @@ class MW_ManualProjectile : Projectile
 				
 				Print(spawnedKnife.GetOrigin());
 				
-				
-				
-				
 				int testBone = 2090329209;
 				other.AddChild(spawnedKnife, testBone);
 				tempFixAlreadySpawned = true;
-				
-				
 				owner.GetPhysics().SetActive(0);
 				owner.SetOrigin("0 0 0");
-				//todo delete it.
-				
-				//array<string> colliderNames;
-				//other.GetAllColliderNames(colliderNames);
-				
-				//SetEventMask(EntityEvent.SIMULATE );
+				//delete owner;
+
 			}
-
-
-			//foreach(string cName : colliderNames)
-			//{
-				
-			
-			
-			//}
-			
-			//mat[0] = contact.Position;
-			
-			//spawnedKnife.SetWorldTransform(mat);
-
-			//spawnedKnife.SetOrigin(contact.Position);
-
-			//spawnedKnife.SetScale(10);
-			
-			//set it invisible or disable it?
-			//delete owner;
-			
 		}
-		
-		
-		
-
-		
-	}
-	
-	override void EOnPhysicsActive(IEntity owner, bool activeState)
-	{		
-		super.EOnPhysicsActive(owner, activeState);
-
-		//Print("Active?");
-	}
-	
-	override void EOnSimulate(IEntity owner, float timeSlice)
-	{
-	
-		//vector vel = addedPhysics.GetVelocity();
-		//
-		//if (vel[0] != 0)
-		//	Print(addedPhysics.GetVelocity());
 
 	}
-	
-	override void EOnFrame(IEntity owner, float timeSlice)
-	{
-		//SetEventMask(EntityEvent.ALL);
-		//Print("cmon");
-		//Physics test = owner.GetPhysics();
-		
-		//test.Destroy();
-		
-		//Physics test2 = Physics.CreateDynamic(owner, 10, EPhysicsLayerPresets.Projectile);
-		
-		//Print(test2.GetVelocity());
-	}
-	
-	
-	
-	
-	
-	override void EOnPostFrame(IEntity owner, float timeSlice)
-	{
-		//Print("PostFrame");
-	}
 
-	
-	
-	
 }
 
